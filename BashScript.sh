2705d75d3a00c1247e408822bd43c275cfbb700d
imtiaza1977 <adeel.imtiaz@gmail.com>
2022-05-29 19:44:17 +0500

    Bash File updated

--- BashScript.sh
+++ BashScript.sh
@@ -3,15 +3,21 @@
 #Connect and Set Subscription Context in Azure
 az login
 az account set --Subscription "Terraform-Deployment"
-RESOURCE_GROUP_NAME=jonnychipz-infra
-STORAGE_ACCOUNT_NAME=jonnychipztstate
+
+#Set Variables for Storage account and Key Vault that Support the Terraform Implementation
+RESOURCE_GROUP_NAME=aws-pre-infra
+STORAGE_ACCOUNT_NAME=aws-pretstate
 CONTAINER_NAME=tstate
+
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location uksouth
+az group create --name $RESOURCE_GROUP_NAME --location KoreaCentral
+
 # Create storage account
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
 # Get storage account key
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
 echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
