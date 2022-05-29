90f5811c6b3f7ac31005aa00db3d168bb55f8830
imtiaza1977 <adeel.imtiaz@gmail.com>
2022-05-29 22:41:41 +0500

    Main File Change

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_storage_account" "aws-presa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags = {
-    environment = "aws-preroxenv"
+    environment = "aws-prerox"
   }
 }
 # Create our vNIC for our VM and assign it to our Virtual Machines Subnet
