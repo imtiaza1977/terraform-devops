813221a782bdbeee07a7406cdf54f2bed1d3b3d3
imtiaza1977 <adeel.imtiaz@gmail.com>
2022-06-01 23:24:54 +0500

    New Main File

--- main.tf
+++ main.tf
@@ -1,94 +1,37 @@
+# 1. Specify the version of the AzureRM Provider to use
 terraform {
-  backend "azurerm" {
-    RESOURCE_GROUP_NAME  = awspre-infra
-    STORAGE_ACCOUNT_NAME = awsprestorage
-    CONTAINER_NAME       = awsprestate
-    key                  = "glhPjhrD1vxHGxTV5+4WZtDz3EPNm6/y720eQkIws15bWhNgQNwGSvNAyhGB7n3P3BlSF7CIuBI0+AStxEuEIQ=="
-  }
-
   required_providers {
     azurerm = {
-      # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 2.4.1"
+      version = "=3.0.1"
     }
   }
 }
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = false
-    }
-  }
-}
-data "azurerm_client_config" "current" {}
 
-# Create our Resource Group - aws-pre-RG
-resource "azurerm_resource_group" "rg" {
-  name     = "aws-pre-app001"
+# 2. Configure the AzureRM Provider
+provider "azurerm" {
+  features {}
+}
+
+#Creating Three Resources
+# 1. Create a resource group
+resource "azurerm_resource_group" "myrg" {
+  name     = "awspre-rg"
   location = "Korea Central"
 }
-# Create our Virtual Network - aws-pre-VNET
-resource "azurerm_virtual_network" "vnet" {
-  name                = "aws-prevnet001"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-# Create our Subnet to hold our VM - Virtual Machines
-resource "azurerm_subnet" "sn" {
-  name                 = "VM001"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-# Create our Azure Storage Account - awspresa
-resource "azurerm_storage_account" "awspresa" {
-  name                     = "awspresa001"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags = {
-    environment = "aws-pre"
-  }
-}
-# Create our vNIC for our VM and assign it to our Virtual Machines Subnet
-resource "azurerm_network_interface" "vmnic" {
-  name                = "aws-prevm001nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.sn.id
-    private_ip_address_allocation = "Dynamic"
-  }
+# 2. Create a virtual network within the resource group
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "aws-pre-vnet"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  address_space       = ["10.0.0.0/16"]
 }
-# Create our Virtual Machine - aws-pre-VM01
-resource "azurerm_virtual_machine" "aws-prevm01" {
-  name                  = "aws-prevm001"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.vmnic.id]
-  vm_size               = "Standard_B1s"
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter-Server-Core-smalldisk"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "aws-prevm001os"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "aws-prevm001"
-    admin_username = "imtiaza"
-    admin_password = "$$Lahore!@#$$"
-  }
-  os_profile_windows_config {
-  }
+
+# 3. Create our Subnet to hold our VM - Virtual Machines
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "aws-pre-subnet"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
